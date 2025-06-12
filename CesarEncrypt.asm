c0b9bb6af3b3dae4975ad46d86652c6e5899a877
Lu <rubertolh07@gmail.com>
2025-06-11 22:47:49 -0300

    Fix about printing cipher

--- CesarEncrypt.asm
+++ CesarEncrypt.asm
@@ -1,33 +1,34 @@
 .data
-	key: .ascii "QWERTYUIOPASDFGHJKLZXCVBNM" 
-	chain: .asciiz "HOLA"
-	cipher: .space 5
-	
+    key: .ascii "QWERTYUIOPASDFGHJKLZXCVBNM"
+    chain: .asciiz "HOLA"
+    cipher: .space 5
+
 .text
-	la $t0, key
-	la $t1, chain
-	la $t2, cipher
-	
-	cifrar:
-  		lb $t3, 0($t1)     
-    	beq $t3, $zero, fin      
+    la $t0, key
+    la $t1, chain
+    la $t2, cipher
 
-    	li $t4, 'A'        
-    	sub $t5, $t3, $t4  
+    cifrar:
+        lb $t3, 0($t1)
+        beq $t3, $zero, fin
 
-    	add $t6, $t0, $t5  
-    	lb $t7, 0($t6)     
+        li $t4, 'A'
+        sub $t5, $t3, $t4
 
-    	sb $t7, 0($t2)     
+        add $t6, $t0, $t5
+        lb $t7, 0($t6)
 
-    	addi $t1, $t1, 1   
-    	addi $t2, $t2, 1   
-    	j cifrar
-		
-	fin:
-    	li $v0, 4
-    	la $a0, cipher
-   		syscall
+        sb $t7, 0($t2)
 
-  		li $v0, 10
-  		syscall
+        addi $t1, $t1, 1
+        addi $t2, $t2, 1
+        j cifrar
+
+    fin:
+        sb $zero, 0($t2)
+        li $v0, 4
+        la $a0, cipher
+        syscall
+
+        li $v0, 10
+        syscall
